2887c7b5f7ce45e813e8a54ee624c85493748704
Xu Che <xuche1998@gmail.com>
2022-08-11 14:17:24 +0800

    added some modelforms

---
+++ auctions/forms.py
@@ -0,0 +1,17 @@
+from .models import Bid, Comment, Listing
+from django.forms import ModelForm
+
+class ListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['title', 'desc', 'image', 'category' , 'user', 'time_created', 'start_time', 'end_time', 'starting_price']
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class BidForm(ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['price']
