0c8d1f2357ec71d3d2cda4d519179617a82100c5
Xu Che <xuche1998@gmail.com>
2022-08-12 15:07:56 +0800

    add some bootstrap styling

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,15 +1,35 @@
 from .models import Bid, Comment, Listing
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea, NumberInput, ClearableFileInput
+
 
 class ListingForm(ModelForm):
     class Meta:
         model = Listing
-        fields = ['title', 'desc', 'image', 'category' , 'starting_price']
+        fields = ['title', 'desc', 'image', 'category', 'starting_price']
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+            }),
+            'desc': Textarea(attrs={
+                'class': "form-control",
+            }),
+            'image': ClearableFileInput(attrs={
+                'class': "form-control",
+            }),
+            'category': TextInput(attrs={
+                'class': "form-control",
+            }),
+            'starting_price': NumberInput(attrs={
+                'class': "form-control",
+            }),
+        }
+
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
 
 class BidForm(ModelForm):
     class Meta:
